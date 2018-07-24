68f7141b27712507125d35a5466f454d281d64f6
praveen <awesomepraveens@gmail.com>
2018-07-24 22:05:50 +0530

    Async Task is used for better network purposes

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,10 +15,13 @@
  */
 package com.example.android.quakereport;
 
+import android.content.AsyncTaskLoader;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -26,20 +29,38 @@ import android.widget.ListView;
 import android.widget.WrapperListAdapter;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=5&limit=10";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+
+        task.execute();
+
+    }
+
+    private void updateUi(ArrayList<Word> extractearthquakes) {
+
         // Create a fake list of earthquake locations.
 
-        final ArrayList<Word> earthquakes = QueryUtils.extractEarthquakes();
+        final ArrayList<Word> earthquakes = extractearthquakes;
 
         // Find a reference to the {@link ListView} in the layout
         final ListView earthquakeListView = (ListView) findViewById(R.id.list);
@@ -70,5 +91,31 @@ public class EarthquakeActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<URL, Void, ArrayList<Word>> {
+        @Override
+        protected ArrayList<Word> doInBackground(URL... urls) {
+            //Create Url object
+            URL url = QueryUtils.createUrl(USGS_REQUEST_URL);
+
+            //Perform Http response to the URL and recieve a JSON response
+            String jsonResponse = "";
+
+            jsonResponse = QueryUtils.makeHttpRequest(url);
+
+            //Extract desirable fields from the JSON Response
+            ArrayList<Word> earthquakes = QueryUtils.extractEarthquakes(jsonResponse);
+            return earthquakes;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Word> earthquakes) {
+            if (earthquakes == null) {
+                return;
+            }
+
+            updateUi(earthquakes);
+        }
     }
 }
